e08858d08906b2a8d8fe1da150bc4683cccf4f0a
memarsh <mariahmartinshein@Serena.local>
2015-02-11 17:19:34 -0400

    List view and second activity created

--- MappPrototype/app/src/main/java/com/example/frontendian/mappprototype/History.java
+++ MappPrototype/app/src/main/java/com/example/frontendian/mappprototype/History.java
@@ -1,17 +1,31 @@
 package com.example.frontendian.mappprototype;
 
+//Much of this code was adapted from http://www.raywenderlich.com/78576/android-tutorial-for-beginners-part-2
+
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 
-public class History extends ActionBarActivity implements View.OnClickListener {
+
+public class History extends ActionBarActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
 
     TextView mainTextView;
     Button mainButton;
+    ListView mainListView;
+    ArrayAdapter mArrayAdapter;
+    ArrayList mNameList = new ArrayList();
+    ShareActionProvider mShareActionProvider;
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +39,21 @@ public class History extends ActionBarActivity implements View.OnClickListener {
         // and listen for it here
         mainButton = (Button) findViewById(R.id.first_switch);
         mainButton.setOnClickListener(this);
+
+        // 4. Access the ListView
+        mainListView = (ListView) findViewById(R.id.main_listview);
+
+        // Create an ArrayAdapter for the ListView
+        // change the layout (second argument) to a customized layout when needed
+        mArrayAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                mNameList);
+
+        // Set the ListView to use the ArrayAdapter
+        mainListView.setAdapter(mArrayAdapter);
+
+        // 5. Set this activity to react to list items being pressed
+        mainListView.setOnItemClickListener(this);
     }
 
 
@@ -39,6 +68,19 @@ public class History extends ActionBarActivity implements View.OnClickListener {
     public void onClick(View v) {
         // Test the Button
         mainTextView.setText("Button pressed!");
+
+        // Also add that value to the list shown in the ListView
+        mNameList.add("Inscription " + counter);
+        mArrayAdapter.notifyDataSetChanged();
+        counter++;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        // Log the item's position and contents
+        // to the console in Debug
+        Log.d("mapp history", position + ": " + mNameList.get(position));
     }
 
     /**@Override
